1065793045107288146e1d189dcc4cf82c7d0c3e
Hempflingclub <29497373+Hempflingclub@users.noreply.github.com>
2023-01-25 21:16:00 +0100

    Finished completed System, .jar Arguments

--- src/main/java/cbzCompress/Main.java
+++ src/main/java/cbzCompress/Main.java
@@ -2,8 +2,6 @@ package cbzCompress;
 
 import cbzCompress.Utils.CbzUtil;
 
-import java.nio.file.Path;
-
 public class Main {
     public static void main(String[] args) {
         //Run CbzUtil for all files in x Folder, and rerun every 30mins with rescanning the folder
@@ -14,16 +12,25 @@ public class Main {
         Compression of Folder into .cbz works
         Extraction function
         Image Compression, in function followed with Compression
-         */
-        //To be done:
-        /*
         Completed System
         .jar arguments, to define the baseFolders, without recompiling
          */
-        String inputPath = "F:\\Projekte\\IntelliJ\\cbzCompress\\compressTest\\Input";
-        String tempPath = "F:\\Projekte\\IntelliJ\\cbzCompress\\compressTest\\Tmp";
-        String outputPath = "F:\\Projekte\\IntelliJ\\cbzCompress\\compressTest\\Out";
-        Path resultArchive = CbzUtil.fullRecompressionOfFirstFile(inputPath, tempPath, outputPath);
-        System.out.println(resultArchive);
+        if (args.length != 4) {
+            System.out.println("Provide 4 Arguments:");
+            System.out.println("{1} Path to inputFolder");
+            System.out.println("{2} Path to tempFolder");
+            System.out.println("{3} Path to outputFolder");
+            System.out.println("{4} Path to finishedFolder");
+            System.exit(0);
+        }
+        String inputPath = args[0];
+        String tempPath = args[1];
+        String outputPath = args[2];
+        String finishedPath = args[3];
+        boolean recompressingStatus = true;
+        while (recompressingStatus) {
+            recompressingStatus = CbzUtil.constantCompressionAction(inputPath, tempPath, outputPath, finishedPath);
+        }
+        System.out.println("Finished");
     }
 }
--- src/main/java/cbzCompress/Utils/CbzUtil.java
+++ src/main/java/cbzCompress/Utils/CbzUtil.java
@@ -3,10 +3,12 @@ package cbzCompress.Utils;
 
 import java.io.File;
 import java.io.IOException;
+import java.nio.file.Files;
 import java.nio.file.Path;
 import java.util.Objects;
 
 import static cbzCompress.Utils.SevenZUtil.constantDelete;
+import static java.lang.Thread.sleep;
 
 abstract public class CbzUtil {
     //Use Utils to solve Minimize CBZ Size, by recompressing after lowering Image Quality to 70
@@ -21,7 +23,44 @@ abstract public class CbzUtil {
         }
     }
 
-    public static Path fullRecompressionOfFirstFile(String targetArchiveFolder, String tempFolder, String destFolder) {
+    private static void wait(int minutes) {
+        int seconds = minutes * 60;
+        int ms = seconds * 1000;
+        try {
+            sleep(ms);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public static boolean constantCompressionAction(String targetArchiveFolder, String tempFolder, String destFolder, String finishedFolder) {
+        wait(1);
+        Path resultArchive = fullRecompressionOfFirstFile(targetArchiveFolder, tempFolder, destFolder);
+        if (resultArchive == null) {
+            return false;
+        }
+        File resultArchiveFile = resultArchive.toFile();
+        String fileName = resultArchiveFile.getName();
+        System.out.println(fileName);
+        Path finishedFolderPath = Path.of(finishedFolder + File.separator + fileName);
+        //Just in case create provided Path of finishedFolder
+        File finishedFolderFile = new File(finishedFolder);
+        finishedFolderFile.mkdirs();
+        //Moving of File
+        try {
+            Files.copy(resultArchive, finishedFolderPath);
+            if (resultArchiveFile.exists()) {
+                while (!resultArchiveFile.delete()) ;
+            }
+        } catch (IOException e) {
+            System.out.println("IOException during moving to finished folder");
+            e.printStackTrace();
+            return false;
+        }
+        return true;
+    }
+
+    private static Path fullRecompressionOfFirstFile(String targetArchiveFolder, String tempFolder, String destFolder) {
         //Just in case create provided Paths
         File targetArchiveFolderFile = new File(targetArchiveFolder);
         File tempFolderFile = new File(tempFolder);
