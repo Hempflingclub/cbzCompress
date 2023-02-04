c669c97191fb70e8c2601d0c2eb3b4322b6272f1
Hempflingclub <29497373+Hempflingclub@users.noreply.github.com>
2023-02-04 07:14:27 +0100

    Gofile link more visible in action

--- .github/scripts/upload.sh
+++ .github/scripts/upload.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 
 # Exit if No File is Specified
-if [[ "$#"  ==  '0' ]]; then
-echo  -e 'ERROR: No File Specified!' && exit 1
+if [[ "$#" == '0' ]]; then
+  echo -e 'ERROR: No File Specified!' && exit 1
 fi
 
 # File to Upload
 FILE="@$1"
 
 # Find the Best server to upload
-SERVER=$(curl -s https://apiv2.gofile.io/getServer | jq  -r '.data|.server')
+SERVER=$(curl -s https://apiv2.gofile.io/getServer | jq -r '.data|.server')
 
 UPLOAD=$(curl -F file=${FILE} https://${SERVER}.gofile.io/uploadFile)
 
@@ -19,3 +19,4 @@ LINK=$(echo $UPLOAD | jq -r '.data|.downloadPage')
 echo $LINK
 
 echo " "
+echo $LINK >'link'
