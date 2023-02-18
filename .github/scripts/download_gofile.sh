4a6a285f852b32c5edfd80088f237d83a96d01a4
Hempflingclub <29497373+Hempflingclub@users.noreply.github.com>
2023-02-18 01:50:06 +0100

    restoring original gofile_download script formatting

--- .github/scripts/download_gofile.sh
+++ .github/scripts/download_gofile.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
 
-id=$(sed -E 's/.*gofile.io\/d\/(\w*?)/\1/' <<<$1)
+id=`sed -E 's/.*gofile.io\/d\/(\w*?)/\1/' <<< $1`
 echo "Downloading $id"
 
-server=$(curl -s https://apiv2.gofile.io/getServer?c=$id | jq -r '.data.server')
-files=$(curl -s https://$server.gofile.io/getUpload?c=$id | jq '.data.files')
+server=`curl -s https://apiv2.gofile.io/getServer?c=$id | jq -r '.data.server'`
+files=`curl -s https://$server.gofile.io/getUpload?c=$id | jq '.data.files'`
 
-for key in $(jq 'keys | .[]' <<<$files); do
-  file=$(jq ".[$key]" <<<$files)
-  url=$(jq -r '.link' <<<$file)
-  name=$(jq -r '.name' <<<$file)
-  echo
-  echo "Downloading $name"
-  curl $url -o "$name"
+for key in `jq 'keys | .[]' <<< $files`; do
+    file=`jq ".[$key]" <<< $files`
+    url=`jq -r '.link' <<< $file`
+    name=`jq -r '.name' <<< $file`
+    echo
+    echo "Downloading $name"
+    curl $url -o "$name"
 done
